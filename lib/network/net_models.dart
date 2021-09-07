98be5ecff74dd818ab4fd9f8e35f3e197480344c
EvgenyAbdullaev <90175056+EvgenyAbdullaev@users.noreply.github.com>
2021-09-07 12:25:56 +0300

    Added network json models

---
+++ lib/network/net_models.dart
@@ -0,0 +1,128 @@
+import 'package:equatable/equatable.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'net_models.g.dart';
+
+@JsonSerializable()
+class APIAllQuery {
+  factory APIAllQuery.fromJson(Map<String, dynamic> json) => _$APIAllQueryFromJson(json);
+  Map<String, dynamic> toJson() => _$APIAllQueryToJson(this);
+
+  List<APIClist> clist;
+  List<APICatalog> catalog;
+
+  APIAllQuery({
+    required this.clist,
+    required this.catalog
+  });
+}
+
+@JsonSerializable()
+class APIClist {
+  factory APIClist.fromJson(Map<String, dynamic> json) =>
+      _$APIClistFromJson(json);
+  Map<String, dynamic> toJson() => _$APIClistToJson(this);
+
+  APICoffee coffee;
+
+  APIClist({
+    required this.coffee
+  });
+}
+
+@JsonSerializable()
+class APICoffee extends Equatable {
+  final int id;
+  final int cat;
+  final String brand;
+  final String label;
+  final double price;
+  final double discount;
+  final int weight;
+  final String img;
+  final String tt;
+  final String drink;
+  final String roast;
+  final String pack;
+  final String compound;
+  final String intensity;
+  final String origin;
+  final String life;
+  final String description;
+
+  APICoffee({
+    required this.id,
+    required this.cat,
+    required this.brand,
+    required this.label,
+    required this.price,
+    required this.discount,
+    required this.weight,
+    required this.img,
+    required this.tt,
+    required this.drink,
+    required this.roast,
+    required this.pack,
+    required this.compound,
+    required this.intensity,
+    required this.origin,
+    required this.life,
+    required this.description
+  });
+
+  factory APICoffee.fromJson(Map<String, dynamic> json) =>
+      _$APICoffeeFromJson(json);
+  Map<String, dynamic> toJson() => _$APICoffeeToJson(this);
+
+  @override
+  List<Object?> get props => [id];
+}
+
+@JsonSerializable()
+class APICatalog {
+  factory APICatalog.fromJson(Map<String, dynamic> json) => _$APICatalogFromJson(json);
+  Map<String, dynamic> toJson() => _$APICatalogToJson(this);
+
+  APICategory category;
+
+  APICatalog({
+    required this.category
+  });
+}
+
+@JsonSerializable()
+class APICategory extends Equatable {
+  factory APICategory.fromJson(Map<String, dynamic> json) => _$APICategoryFromJson(json);
+  Map<String, dynamic> toJson() => _$APICategoryToJson(this);
+
+  final int id;
+  final String name;
+  final String img;
+
+  APICategory({
+    required this.id,
+    required this.name,
+    required this.img
+  });
+
+  @override
+  List<Object?> get props => [id];
+}
+
+@JsonSerializable()
+class APISimpleAnswer {
+  factory APISimpleAnswer.fromJson(Map<String, dynamic> json) => _$APISimpleAnswerFromJson(json);
+  Map<String, dynamic> toJson() => _$APISimpleAnswerToJson(this);
+
+  final String ans;
+  final String ver;
+  final String sic;
+
+  APISimpleAnswer({
+    required this.ans,
+    required this.ver,
+    required this.sic
+  });
+}
+
+
---
+++ lib/network/net_models.g.dart
@@ -0,0 +1,117 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'net_models.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+APIAllQuery _$APIAllQueryFromJson(Map<String, dynamic> json) {
+  return APIAllQuery(
+    clist: (json['clist'] as List<dynamic>)
+        .map((e) => APIClist.fromJson(e as Map<String, dynamic>))
+        .toList(),
+    catalog: (json['catalog'] as List<dynamic>)
+        .map((e) => APICatalog.fromJson(e as Map<String, dynamic>))
+        .toList(),
+  );
+}
+
+Map<String, dynamic> _$APIAllQueryToJson(APIAllQuery instance) =>
+    <String, dynamic>{
+      'clist': instance.clist,
+      'catalog': instance.catalog,
+    };
+
+APIClist _$APIClistFromJson(Map<String, dynamic> json) {
+  return APIClist(
+    coffee: APICoffee.fromJson(json['coffee'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$APIClistToJson(APIClist instance) => <String, dynamic>{
+  'coffee': instance.coffee,
+};
+
+APICoffee _$APICoffeeFromJson(Map<String, dynamic> json) {
+  return APICoffee(
+    id: json['id'] as int,
+    cat: json['cat'] as int,
+    brand: json['brand'] as String,
+    label: json['label'] as String,
+    price: (json['price'] as num).toDouble(),
+    discount: (json['discount'] as num).toDouble(),
+    weight: json['weight'] as int,
+    img: json['img'] as String,
+    tt: json['tt'] as String,
+    drink: json['drink'] as String,
+    roast: json['roast'] as String,
+    pack: json['pack'] as String,
+    compound: json['compound'] as String,
+    intensity: json['intensity'] as String,
+    origin: json['origin'] as String,
+    life: json['life'] as String,
+    description: json['description'] as String,
+  );
+}
+
+Map<String, dynamic> _$APICoffeeToJson(APICoffee instance) => <String, dynamic>{
+  'id': instance.id,
+  'cat': instance.cat,
+  'brand': instance.brand,
+  'label': instance.label,
+  'price': instance.price,
+  'discount': instance.discount,
+  'weight': instance.weight,
+  'img': instance.img,
+  'tt': instance.tt,
+  'drink': instance.drink,
+  'roast': instance.roast,
+  'pack': instance.pack,
+  'compound': instance.compound,
+  'intensity': instance.intensity,
+  'origin': instance.origin,
+  'life': instance.life,
+  'description': instance.description,
+};
+
+APICatalog _$APICatalogFromJson(Map<String, dynamic> json) {
+  return APICatalog(
+    category: APICategory.fromJson(json['category'] as Map<String, dynamic>),
+  );
+}
+
+Map<String, dynamic> _$APICatalogToJson(APICatalog instance) =>
+    <String, dynamic>{
+      'category': instance.category,
+    };
+
+APICategory _$APICategoryFromJson(Map<String, dynamic> json) {
+  return APICategory(
+    id: json['id'] as int,
+    name: json['name'] as String,
+    img: json['img'] as String,
+  );
+}
+
+Map<String, dynamic> _$APICategoryToJson(APICategory instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+      'img': instance.img,
+    };
+
+APISimpleAnswer _$APISimpleAnswerFromJson(Map<String, dynamic> json) {
+  return APISimpleAnswer(
+    ans: json['ans'] as String,
+    ver: json['ver'] as String,
+    sic: json['sic'] as String,
+  );
+}
+
+Map<String, dynamic> _$APISimpleAnswerToJson(APISimpleAnswer instance) =>
+    <String, dynamic>{
+      'ans': instance.ans,
+      'ver': instance.ver,
+      'sic': instance.sic,
+    };
